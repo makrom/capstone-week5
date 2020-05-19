de9178306f71494ac4a8737e6a465d29088d8940
makrom <makrom61@gmail.com>
2020-05-19 16:44:47 +0300

    assets precompiled for production

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -1,4 +1,5 @@
 Devise.setup do |config|
   #config.email_regexp = /\A[^@\s]+@[^@\s]+\z/
   config.navigational_formats = [:json]
+  config.secret_key = '9121a0eab72edbeea00afe055509c92c404dcb21743d766b45345f0a1f476d094da0c971970fccd274675e21d73aff0451866a099b81fab18f53b4fb7c322579'
 end
