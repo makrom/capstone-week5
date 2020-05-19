1f4965bd6981b2679c309bf2cba9ee1914a12562
makrom <makrom61@gmail.com>
2020-05-19 16:25:12 +0300

    user image features added

--- app/assets/javascripts/spa-demo/authn/signup/signup.component.js
+++ app/assets/javascripts/spa-demo/authn/signup/signup.component.js
@@ -12,13 +12,14 @@
   templateUrl.$inject = ["spa-demo.config.APP_CONFIG"];
   function templateUrl(APP_CONFIG) {
     return APP_CONFIG.authn_signup_html;
-  }    
+  }
 
-  SignupController.$inject = ["$scope","$state","spa-demo.authn.Authn"];
-  function SignupController($scope, $state, Authn) {
+  SignupController.$inject = ["$scope","$state","spa-demo.authn.Authn", "spa-demo.layout.DataUtils", "spa-demo.subjects.Image"];
+  function SignupController($scope, $state, Authn, DataUtils, Image) {
     var vm=this;
     vm.signupForm = {}
     vm.signup = signup;
+    vm.setImageContent = setImageContent;
 
     vm.$onInit = function() {
       console.log("SignupController",$scope);
@@ -31,15 +32,34 @@
       Authn.signup(vm.signupForm).then(
         function(response){
           vm.id = response.data.data.id;
-          console.log("signup complete", response.data, vm);          
-          $state.go("home");
+          console.log("signup complete", response.data, vm);
+          if (vm.image_content) {
+            var userId = response.data.data.id;
+            var image = new Image();
+            image.user_id = userId;
+            image.image_content = {};
+            image.image_content.content_type = "image/jpeg"
+            image.image_content.content = vm.image_content.content;
+            image.$save().then(
+              function(result){
+                $state.go("home");
+              },
+              function(error){
+                console.log("Failed to upload image");
+                $state.go("home");
+              });
+          }
+/*          $state.go("home");*/
         },
         function(response){
           vm.signupForm["errors"]=response.data.errors;
-          console.log("signup failure", response, vm);          
+          console.log("signup failure", response, vm);
         }
       );
     }
 
+        function setImageContent(dataUri) {
+          vm.image_content = DataUtils.getContentFromDataUri(dataUri);
+        }
   }
 })();
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170130051702) do
+ActiveRecord::Schema.define(version: 20200519084505) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -45,6 +45,15 @@ ActiveRecord::Schema.define(version: 20170130051702) do
   add_index "roles", ["mname"], name: "index_roles_on_mname", using: :btree
   add_index "roles", ["user_id"], name: "index_roles_on_user_id", using: :btree
 
+  create_table "tags", force: :cascade do |t|
+    t.string   "name"
+    t.integer  "creator_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "tags", ["creator_id"], name: "index_tags_on_creator_id", using: :btree
+
   create_table "thing_images", force: :cascade do |t|
     t.integer  "image_id",               null: false
     t.integer  "thing_id",               null: false
@@ -57,6 +66,18 @@ ActiveRecord::Schema.define(version: 20170130051702) do
   add_index "thing_images", ["image_id", "thing_id"], name: "index_thing_images_on_image_id_and_thing_id", unique: true, using: :btree
   add_index "thing_images", ["image_id"], name: "index_thing_images_on_image_id", using: :btree
   add_index "thing_images", ["thing_id"], name: "index_thing_images_on_thing_id", using: :btree
+
+  create_table "thing_tags", force: :cascade do |t|
+    t.integer  "thing_id",   null: false
+    t.integer  "tag_id",     null: false
+    t.integer  "creator_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "thing_tags", ["tag_id", "thing_id"], name: "index_thing_tags_on_tag_id_and_thing_id", unique: true, using: :btree
+  add_index "thing_tags", ["tag_id"], name: "index_thing_tags_on_tag_id", using: :btree
+  add_index "thing_tags", ["thing_id"], name: "index_thing_tags_on_thing_id", using: :btree
 
   create_table "things", force: :cascade do |t|
     t.string   "name",        null: false
@@ -91,13 +112,18 @@ ActiveRecord::Schema.define(version: 20170130051702) do
     t.json     "tokens"
     t.datetime "created_at",                               null: false
     t.datetime "updated_at",                               null: false
+    t.integer  "image_id"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", using: :btree
+  add_index "users", ["image_id"], name: "index_users_on_image_id", using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   add_index "users", ["uid", "provider"], name: "index_users_on_uid_and_provider", unique: true, using: :btree
 
   add_foreign_key "roles", "users"
   add_foreign_key "thing_images", "images"
   add_foreign_key "thing_images", "things"
+  add_foreign_key "thing_tags", "tags"
+  add_foreign_key "thing_tags", "things"
+  add_foreign_key "users", "images"
 end
